b412dfcc0f2ec10114f1f9ee32ec91cb99ba9169
TeamLightning <>
2021-05-08 05:32:21 +0000

    approved in shardinfo

--- cogs/sharding.py
+++ cogs/sharding.py
@@ -7,6 +7,9 @@ class Sharding(commands.Cog):
   
   @commands.command()
   async def shardinfo(self, ctx):
+    approved = ['547941645304201247', '743549337434587327']
+    if str(ctx.author.id) not in approved:
+      return
     shards = self.bot.shards
     for i in range(len(shards)):
       await ctx.send(f'Shard {i} has latency {round(shards[i].latency * 1000, 3)}ms')
